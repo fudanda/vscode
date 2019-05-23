921eff541abdb16cdace7d8d94cc431f9c8515d8
huo san <314553951@qq.com>
2019-05-23 17:09:18 +0800

    feat:上传完整代码

---
+++ doc.md
@@ -0,0 +1,7 @@
+# 插件 
+    custom css and js load
+    synthwave,84
+    
+    Ctrl + Shift + P，运行 "Reload Custom CSS and JS", 
+
+    配置文件 settings.json
---
+++ enable-electron-vibrancy.js
@@ -0,0 +1,2 @@
+nodeRequire('electron').remote.getCurrentWindow().setVibrancy('ultra-dark');
+nodeRequire('electron').remote.getCurrentWindow().setOpacity(0.8);
